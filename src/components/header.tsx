fed561eca72eba27240d5c48f901ba999c98b5d7
Humberd <humberd2@gmail.com>
2019-11-17 22:38:06 +0100

    Added openModal callback

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,18 @@ import { FaPlus } from 'react-icons/fa';
 import './header.scss';
 
 export const AppHeader: React.FC = () => {
+  const openModal = () => {
+    // @ts-ignore
+  };
+
   return (
       <header className="AppHeader">
         <h2>Users</h2>
 
-        <button className="app-button new">
+        <button
+            className="app-button new"
+            onClick={openModal}
+        >
           New
           <FaPlus/>
         </button>

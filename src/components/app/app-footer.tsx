118e74a14a267d487cd7970f9996986e8d608609
Humberd <humberd2@gmail.com>
2019-11-24 19:59:13 +0100

    Updated footer

--- src/components/app/app-footer.tsx
+++ src/components/app/app-footer.tsx
@@ -4,7 +4,8 @@ import './app-footer.scss';
 export const AppFooter: React.FC = () => {
   return (
       <footer className="top-level-container AppFooter">
-        <span>Created by Maciej Sawicki</span>
+        <span>Created by <strong>Maciej Sawicki</strong></span>
+        <span>Designs by <strong>Marta Zajkowska</strong></span>
       </footer>
   );
 };

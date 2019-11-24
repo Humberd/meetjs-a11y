a00b8df7d16bb9f9f00a0b6bf30097fe7b374b9e
Humberd <humberd2@gmail.com>
2019-11-24 19:59:25 +0100

    Fix

--- src/components/app/app-footer.tsx
+++ src/components/app/app-footer.tsx
@@ -5,7 +5,7 @@ export const AppFooter: React.FC = () => {
   return (
       <footer className="top-level-container AppFooter">
         <span>Created by <strong>Maciej Sawicki</strong></span>
-        <span>Designs by <strong>Marta Zajkowska</strong></span>
+        <span>Desiged by <strong>Marta Zajkowska</strong></span>
       </footer>
   );
 };
